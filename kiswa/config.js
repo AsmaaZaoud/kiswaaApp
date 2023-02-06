871ffbf743a23173d0fb49d6c69fa6ee1fb0fcb5
AsmaaZaoud <99315884+AsmaaZaoud@users.noreply.github.com>
2023-02-06 17:01:51 +0300

    Create config.js

---
+++ kiswa/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPLEdfCMZhdgmqKTFBmoJ18EeTOpNssyI",
+  authDomain: "kiswa-161b6.firebaseapp.com",
+  projectId: "kiswa-161b6",
+  storageBucket: "kiswa-161b6.appspot.com",
+  messagingSenderId: "328139468827",
+  appId: "1:328139468827:web:ac710e6c1fd7555ca848be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
