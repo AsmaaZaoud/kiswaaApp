74b1f3d5b7778b91588f8631257702462f88957f
AsmaaZaoud <99315884+AsmaaZaoud@users.noreply.github.com>
2023-03-18 02:27:28 +0300

    update driver screens
    add listner

--- kiswa/screens/Asmaa/DriverHome.js
+++ kiswa/screens/Asmaa/DriverHome.js
@@ -70,23 +70,22 @@ const DriverHome = (props) => {
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       console.log("snapshot");
       setArr(
-        querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
+        querySnapshot.docs.map((doc) =>
+          doc.data().type == "pickup" && doc.data().status == "pending"
+            ? doc.data()
+            : undefined
+        )
       );
-      // setArr(querySnapshot.docs.map((doc) => doc.data()));
-      // console.log(cart);
-      // filterOrders();
-    });
 
-    console.log("oooooooo", arr[0].data.location);
+      setOrders(querySnapshot.docs.map((doc) => doc.data()));
+      // setArr(orders.filter((x) => x.type == "pickup" && x.status == "pending"));
+      console.log(arr);
+    });
 
     return () => unsubscribe();
   };
   const id = props.email;
   const navigation = props.navigation;
-  // alert(id);
   const [deviceType, setDeviceType] = useState("");
   const [type, setType] = useState("pick");
   const [arr, setArr] = useState([]);
@@ -97,82 +96,25 @@ const DriverHome = (props) => {
   const [index, setIndex] = useState(0);
 
   const change = (type) => {
+    console.log("changeeee", orders);
     if (type == "deliv") {
       setType("deliv");
-      setArr(deliver);
+
+      setArr(
+        orders.filter((x) => x.type == "deliver" && x.status == "pending")
+      );
     } else {
       setType("pick");
-      setArr(pickup);
+      setArr(orders.filter((x) => x.type == "pickup" && x.status == "pending"));
     }
   };
 
-  const filterOrders = () => {
-    var temp = [...orders];
-    console.log("temp", temp);
-    var pick = [];
-    var deliv = [];
-    temp.forEach((x) => {
-      x.type == "pickup" ? pick.push(x) : deliv.push(x);
-    });
-    setPickup(pick);
-    setDeliver(deliv);
-    setArr(pick);
-  };
   const [orders, setOrders] = useState([]);
-  const [pickup, setPickup] = useState([]);
-  const [deliver, setDeliver] = useState([]);
-  const [data, setData] = useState();
+
   let user = auth?.currentUser?.email;
 
-  // const readOrders = async () => {
-  //   let temp = [];
-  //   let pick = [];
-  //   let deliv = [];
-  //   console.log(id.toLowerCase());
-
-  //   const q = query(collection(db, "drivers", user, "orders"));
-  //   // console.log("qqqq", q);
-
-  //   const docs = await getDocs(q);
-  //   // console.log("docs", docs);
-  //   docs.forEach(async (doc) => {
-  //     var num = doc.id[7];
-  //     let hour = doc.data().dateTime.toDate().getHours();
-  //     let t = doc.data();
-  //     t.time = hour + ":00";
-  //     t.date = doc.data().dateTime.toDate().toLocaleDateString();
-  //     let a;
-
-  //     doc.data().type == "pickup"
-  //       ? (a = await readUser(doc.data().userId, "donors"))
-  //       : (a = await readUser(doc.data().userId, "families"));
-  //     t.userName = a.userName;
-  //     t.phone = a.phone;
-  //     t.id = doc.id;
-  //     t.num = doc.id.split(num)[0];
-  //     if (t.status == "pending") {
-  //       temp.push(t);
-  //       doc.data().type == "pickup" ? pick.push(t) : deliv.push(t);
-  //     }
-
-  //     setOrders(temp);
-  //     setPickup(pick);
-  //     setDeliver(deliv);
-  //     setArr(pickup);
-  //   });
-  // };
   let lat = 25.2709954;
   let long = 51.5324509;
-
-  const readUser = async (id, table) => {
-    const docRef = doc(db, table, id);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      return docSnap.data();
-    } else {
-      console.log("No such document!");
-    }
-  };
 
   return (
     <View>
@@ -194,122 +136,141 @@ const DriverHome = (props) => {
         <ScrollView>
           <View style={styles.home}>
             {arr.length != 0 ? (
-              arr.map((x) => (
-                <View key={x.id} style={styles.card}>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Text style={styles.cardTitle}>Order No</Text>
-
-                    {/* <Text style={styles.cardTitle}>#{x.num}</Text> */}
-                  </View>
-                  <View style={styles.userCard}>
-                    <FontAwesome name="user-circle-o" size={50} />
-                    <View style={{ marginLeft: 10 }}>
-                      <Text
-                        style={{ fontSize: normalize(15), fontWeight: "bold" }}
-                      >
-                        Name
-                      </Text>
-                      <Text
-                        style={{ fontSize: normalize(15), fontWeight: "bold" }}
-                      >
-                        Phone
-                      </Text>
-                      <Text
-                        style={{ fontSize: normalize(15), fontWeight: "bold" }}
-                      >
-                        Email
-                      </Text>
-                    </View>
-                    <View style={{ marginLeft: 10 }}>
-                      <Text style={{ fontSize: normalize(15) }}>
-                        {/* {x.userName} */}
-                        aaa
-                      </Text>
-                      <Text style={{ fontSize: normalize(15) }}>
-                        {/* {x.phone}  */}
-                        444
-                      </Text>
-                      <Text style={{ fontSize: normalize(15) }}>
-                        {x.userId}
-                      </Text>
-                    </View>
-                  </View>
-
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      marginTop: 15,
-                    }}
-                  >
-                    <View style={[styles.dataView, { flexDirection: "row" }]}>
-                      <Ionicons
-                        name="md-today-sharp"
-                        size={30}
-                        color="#5e1e7f"
-                      />
-                      <Text style={styles.dataTitles}>{x.date}</Text>
-                    </View>
-                    <View style={[styles.dataView, { flexDirection: "row" }]}>
-                      <Ionicons name="time-outline" size={30} color="#5e1e7f" />
-                      <Text style={styles.dataTitles}>{x.time} PM</Text>
-                    </View>
-                  </View>
-
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      marginTop: 15,
-                    }}
-                  >
-                    <View style={[styles.dataView, { flexDirection: "row" }]}>
-                      <Ionicons
-                        name="location-outline"
-                        size={30}
-                        color="#5e1e7f"
-                      />
-                      <Text style={styles.dataTitles}>{x.location}</Text>
-                    </View>
-                    <View style={[styles.dataView, { flexDirection: "row" }]}>
-                      <Ionicons name="map-outline" size={30} color="#5e1e7f" />
-                      <Text
-                        style={styles.dataTitles}
-                        onPress={() =>
-                          Linking.openURL(
-                            `https://www.google.com/maps/search/?api=1&query=${lat},${long}`
-                          )
-                        }
-                      >
-                        Open Map
-                      </Text>
-                    </View>
-                  </View>
-
-                  <View
-                    style={{ justifyContent: "center", alignItems: "center" }}
-                  >
-                    <Pressable
-                      onPress={() => navigation.navigate("OrderDetails", x.id)}
-                      style={styles.pickupButtonContainer}
+              arr.map((x) =>
+                x != undefined ? (
+                  <View key={x.id} style={styles.card}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                      }}
                     >
-                      {type == "pick" ? (
-                        <Text style={styles.pickupButton}>Pick up</Text>
-                      ) : (
-                        <Text style={styles.pickupButton}>Deliver</Text>
-                      )}
-                    </Pressable>
-                    {/* <Pressable style={styles.cancelButtonContainer}>
+                      <Text style={styles.cardTitle}>Order No</Text>
+
+                      {/* <Text style={styles.cardTitle}>#{x.num}</Text> */}
+                    </View>
+                    <View style={styles.userCard}>
+                      <FontAwesome name="user-circle-o" size={50} />
+                      <View style={{ marginLeft: 10 }}>
+                        <Text
+                          style={{
+                            fontSize: normalize(15),
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Name
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: normalize(15),
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Phone
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: normalize(15),
+                            fontWeight: "bold",
+                          }}
+                        >
+                          Email
+                        </Text>
+                      </View>
+                      <View style={{ marginLeft: 10 }}>
+                        <Text style={{ fontSize: normalize(15) }}>
+                          {x.userName}
+                        </Text>
+                        <Text style={{ fontSize: normalize(15) }}>
+                          {x.phone}
+                        </Text>
+                        <Text style={{ fontSize: normalize(15) }}>
+                          {x.userId}
+                        </Text>
+                      </View>
+                    </View>
+
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        marginTop: 15,
+                      }}
+                    >
+                      <View style={[styles.dataView, { flexDirection: "row" }]}>
+                        <Ionicons
+                          name="md-today-sharp"
+                          size={30}
+                          color="#5e1e7f"
+                        />
+                        <Text style={styles.dataTitles}>{x.date}</Text>
+                      </View>
+                      <View style={[styles.dataView, { flexDirection: "row" }]}>
+                        <Ionicons
+                          name="time-outline"
+                          size={30}
+                          color="#5e1e7f"
+                        />
+                        <Text style={styles.dataTitles}>{x.timeSlot}</Text>
+                      </View>
+                    </View>
+
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        marginTop: 15,
+                      }}
+                    >
+                      <View style={[styles.dataView, { flexDirection: "row" }]}>
+                        <Ionicons
+                          name="location-outline"
+                          size={30}
+                          color="#5e1e7f"
+                        />
+                        <Text style={styles.dataTitles}>{x.location}</Text>
+                      </View>
+                      <View style={[styles.dataView, { flexDirection: "row" }]}>
+                        <Ionicons
+                          name="map-outline"
+                          size={30}
+                          color="#5e1e7f"
+                        />
+                        <Text
+                          style={styles.dataTitles}
+                          onPress={() =>
+                            Linking.openURL(
+                              `https://www.google.com/maps/search/?api=1&query=${lat},${long}`
+                            )
+                          }
+                        >
+                          Open Map
+                        </Text>
+                      </View>
+                    </View>
+
+                    <View
+                      style={{ justifyContent: "center", alignItems: "center" }}
+                    >
+                      <Pressable
+                        onPress={() =>
+                          navigation.navigate("OrderDetails", x.id)
+                        }
+                        style={styles.pickupButtonContainer}
+                      >
+                        {type == "pick" ? (
+                          <Text style={styles.pickupButton}>Pick up</Text>
+                        ) : (
+                          <Text style={styles.pickupButton}>Deliver</Text>
+                        )}
+                      </Pressable>
+                      {/* <Pressable style={styles.cancelButtonContainer}>
             <Text style={styles.cancelButton}>c</Text>
           </Pressable> */}
+                    </View>
                   </View>
-                </View>
-              ))
+                ) : null
+              )
             ) : (
               <Text style={styles.noOrder}> No Orders yet</Text>
             )}
